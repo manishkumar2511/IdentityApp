0dbcf5264972aaaffd3d84c67519c85897a14997
Agilsys <agilsyssoftware@gmail.com>
2024-09-20 09:53:40 +0530

    RxJs learning first commit

---
+++ ClientApp/src/app/rxjs-learning/rxjs-learning.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rxjs-learning',
+  templateUrl: './rxjs-learning.component.html',
+  styleUrl: './rxjs-learning.component.css'
+})
+export class RxjsLearningComponent {
+
+}
